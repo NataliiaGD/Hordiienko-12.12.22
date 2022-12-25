b44304efc087a8954eb2996f405896fb0078175a
NataliiaGD <nataliagordiyenko8@gmail.com>
2022-12-25 21:53:30 +0200

    initial commit

--- src/Hometask4/Triangle.java
+++ src/Hometask4/Triangle.java
@@ -2,13 +2,13 @@ package Hometask4;
 
 public class Triangle {
     public static void main(String[] args) {
-        System.out.println(triangle(4,4,6));
+        System.out.println(triangle(4,6,4));
 
     }
 
     public static boolean triangle (int a,int b,int c){
 
-        if (a == b && a != c && b != c) {
+        if (a == b || a == c || b == c) {
            return true;
         }
         return false;
