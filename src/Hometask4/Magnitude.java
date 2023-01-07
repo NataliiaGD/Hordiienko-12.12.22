91a5d871f899119765244b39b114b9cc5d6be040
NataliiaGD <nataliagordiyenko8@gmail.com>
2023-01-07 20:08:33 +0200

    initial commit

--- src/Hometask4/Magnitude.java
+++ src/Hometask4/Magnitude.java
@@ -2,20 +2,20 @@ package Hometask4;
 
 public class Magnitude {
     public static void main(String[] args) {
-        System.out.println(Math(-9,-8 ));
+        System.out.println(compareMagnitude(-9,-8 ));
     }
 
-    public static String Math (int a,int b){
+    public static String compareMagnitude (int a,int b){
 
         String str = " has bigger magnitude";
 
         if (Math.abs(a) > Math.abs(b)) {
             return a + str;
         }
-        if (Math.abs(a) < Math.abs(b)) {
+        else if (Math.abs(a) < Math.abs(b)) {
             return b + str;
         }
-        if (Math.abs(a) == Math.abs(b)){
+        else if (Math.abs(a) == Math.abs(b)){
             return "numbers are equal";
         }
         return str;
