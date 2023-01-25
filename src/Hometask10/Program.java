0171b83861d644748e75779ad5c585e1e3101b56
NataliiaGD <nataliagordiyenko8@gmail.com>
2023-01-25 12:46:16 +0200

    made changes according to the comments

---
+++ src/Hometask10/Program.java
@@ -0,0 +1,25 @@
+package Hometask10;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.TreeMap;
+
+public class Program {
+    public static void main(String[] args) {
+
+        Map<Integer,String> zoo = new HashMap<>();
+        zoo.put(1,"monkey");
+        zoo.put(2,"tiger");
+        zoo.put(3,"zebra");
+        zoo.put(4,"pig");
+        zoo.put(5,"straus");
+
+        sortValues(zoo);
+
+
+    }
+
+    private static void sortValues(Map<Integer, String> zoo) {
+        zoo.values().stream().sorted().forEach(System.out::println);
+    }
+}
